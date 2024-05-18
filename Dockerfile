57d5f4db7326d3b2d8358768ab28a0a61bbae27f
sachin-oraon_M2P <sachin.oraon@m2pfintech.com>
2024-05-19 02:46:08 +0530

    add telegram search api

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
-FROM python:3.9.0-slim
+FROM python:3.10.0
 COPY . /usr/src/app
 WORKDIR /usr/src/app
 RUN python -m pip install --upgrade pip
 RUN python -m pip install --no-cache-dir -r requirements.txt
+EXPOSE 8000
+CMD ["gunicorn", "--bind" , ":8000", "--workers", "1", "main:flask_app"]
--- main.py
+++ main.py
@@ -1,76 +1,40 @@
-import os
-import re
+import asyncio
+import uvloop
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 import io
-import pickle
+import json
 import logging
+import os
+import pickle
+import re
 import humanize
 import requests
-import json
-import asyncio
-from flask import Flask
-from flask_cors import CORS
+from sys import exit
 from urllib.parse import unquote
 from dotenv import load_dotenv
-from imdb import Cinemagoer
-from google.auth.transport.requests import Request
+from flask import Flask
+from flask_cors import CORS
 from google.auth.exceptions import RefreshError
+from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
-from html_data import HTML_DATA
+from imdb import Cinemagoer
+from natsort import natsorted
+from typing import Optional, Dict
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 CONFIG_FILE_URL = os.getenv("CONFIG_FILE_URL")
-DLWORKER_LIST = list()
-PICKLE_FILE_NAME = "token.pickle"
+PICKLE_FILE = "token.pickle"
 MAX_RESULTS = 30
-REMOVE_DUPLICATES = False
 QUALITIES = ["360", "480", "720", "1080", "1440", "2160"]
-HTML_PAGE = HTML_DATA
+TG_ARCHIVE_SEARCH_API = None
+TG_ARCHIVE_AUTH_API = None
+TG_ARCHIVE_TOKEN = None
+TG_ARCHIVE_USER = None
 
-flask_app = Flask(__name__)
-CORS(flask_app)
-
-def setup_config() -> bool:
-    global HTML_PAGE
-    global DLWORKER_LIST
-    global MAX_RESULTS
-    global REMOVE_DUPLICATES
-    if CONFIG_FILE_URL is not None:
-        logger.info("Downloading config file")
-        try:
-            config_file = requests.get(url=CONFIG_FILE_URL)
-            if config_file.ok:
-                with open('config.env', 'wt', encoding='utf-8') as f:
-                    f.write(config_file.text)
-                logger.info("Loading config values")
-                if load_dotenv('config.env', override=True):
-                    pickle_file = requests.get(url=os.environ['PICKLE_FILE_URL'])
-                    if pickle_file.ok:
-                        with open(PICKLE_FILE_NAME, 'wb') as f:
-                            f.write(pickle_file.content)
-                        MAX_RESULTS = int(os.environ['MAX_RESULTS'])
-                        REMOVE_DUPLICATES = os.environ['REMOVE_DUPLICATES'].lower() == "true"
-                        DLWORKER_LIST = json.loads(os.environ['DLWORKER_LIST'])
-                        if len(DLWORKER_LIST) != 3:
-                            logger.error("Provide 3 download worker URL")
-                        else:
-                            for index, url in enumerate(DLWORKER_LIST):
-                                HTML_PAGE = HTML_PAGE.replace(f'DL_WORKER_{index + 1}', url)
-                            logger.info("Config setup completed")
-                            return True
-                    else:
-                        logger.error(f"Failed to download {PICKLE_FILE_NAME}")
-            else:
-                logger.error("Error while downloading config file")
-        except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError, IndexError, KeyError, json.JSONDecodeError) as err:
-            logger.error(f"Failed to setup config: {str(err)}")
-    else:
-        logger.error("CONFIG_FILE_URL is None")
-    return False
-
-if setup_config() is False:
-    logger.error("Error setting up config")
+class GdriveSearchError(Exception):
+    pass
 
 def isDownloadable(fileId, creds, shared_list) -> None:
     try:
@@ -83,14 +47,16 @@ def isDownloadable(fileId, creds, shared_list) -> None:
     except Exception:
         shared_list.append(fileId)
 
-async def filter_files(fileId, creds, shared_list):
+async def filter_files(fileId, creds, shared_list, is_tgapi):
+    if is_tgapi:
+        return
     await asyncio.to_thread(isDownloadable, fileId, creds, shared_list)
 
-async def check_download(creds, files) -> None:
+async def check_download(creds, resultsMap) -> None:
     shared_list = list()
-    await asyncio.gather(*[asyncio.create_task(filter_files(fileId, creds, shared_list)) for fileId in files])
+    await asyncio.gather(*[asyncio.create_task(filter_files(fileId, creds, shared_list, resultsMap[fileId]['is_tgapi'])) for fileId in resultsMap])
     for fileId in shared_list:
-        files.pop(fileId)
+        resultsMap.pop(fileId)
     del shared_list
 
 def contains(substr: list, fullstr: str) -> bool:
@@ -99,40 +65,174 @@ def contains(substr: list, fullstr: str) -> bool:
             return False
     return True
 
-def get_oauth_creds():
-    logger.info("Loading token.pickle file")
-    with open(PICKLE_FILE_NAME, 'rb') as f:
-        credentials = pickle.load(f)
-        if credentials and credentials.expired and credentials.refresh_token:
-            try:
-                credentials.refresh(Request())
-            except RefreshError:
-                logger.error("Failed to refresh token")
-                return None
-        return credentials
+def setup_config() -> None:
+    global TG_ARCHIVE_USER
+    global TG_ARCHIVE_SEARCH_API
+    global TG_ARCHIVE_AUTH_API
+    IS_CONFIG_OK = False
+    if CONFIG_FILE_URL is not None:
+        logger.info("Downloading config file")
+        try:
+            config_file = requests.get(url=CONFIG_FILE_URL)
+            if config_file.ok:
+                with open('config.env', 'wt', encoding='utf-8') as f:
+                    f.write(config_file.text)
+                logger.info("Loading config values")
+                if load_dotenv('config.env', override=True):
+                    TG_ARCHIVE_SEARCH_API = os.getenv(key='TG_ARCHIVE_SEARCH_API', default='').rstrip('/')
+                    TG_ARCHIVE_AUTH_API = os.getenv(key='TG_ARCHIVE_AUTH_API', default='').rstrip('/')
+                    TG_ARCHIVE_USER = os.getenv(key='TG_ARCHIVE_USER', default=None)
+                    pickle_file = requests.get(url=os.environ['PICKLE_FILE_URL'])
+                    if pickle_file.ok:
+                        with open(PICKLE_FILE, 'wb') as f:
+                            f.write(pickle_file.content)
+                        IS_CONFIG_OK = True
+                    else:
+                        raise requests.exceptions.HTTPError
+            else:
+                logger.error("Error while downloading config file")
+        except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError, KeyError, json.JSONDecodeError):
+            logger.error("Failed to setup config")
+    else:
+        logger.error("CONFIG_FILE_URL is None")
+    if not IS_CONFIG_OK:
+        exit(os.EX_CONFIG)
+
+setup_config()
+flask_app = Flask(__name__)
+CORS(flask_app)
 
 @flask_app.get('/')
-def home() -> str:
-    return HTML_PAGE
+def hello():
+    return 'Hello from File Search Service'
+
+def get_tg_auth_data() -> Dict:
+    auth_data = {
+        "id": TG_ARCHIVE_USER,
+        "first_name": os.getenv(key="FIRST_NAME"),
+        "username": os.getenv(key="USER_NAME"),
+        "auth_date": os.getenv(key="AUTH_DATE"),
+        "hash": os.getenv(key="HASH")
+    }
+    if LAST_NAME := os.getenv(key="LAST_NAME"):
+        auth_data["last_name"] = LAST_NAME
+    if PHOTO_URL := os.getenv(key="PHOTO_URL"):
+        auth_data["photo_url"] = PHOTO_URL
+    return auth_data
+
+def fetch_tgarchive_token() -> Optional[str]:
+    auth_token = None
+    if not TG_ARCHIVE_AUTH_API or not TG_ARCHIVE_USER:
+        logger.warning("Required params for fetching token are missing")
+        return auth_token
+    logger.info(f"Fetching token from:: {TG_ARCHIVE_AUTH_API}")
+    try:
+        req_body = {
+            "site": "tgarchive",
+            "user_id": TG_ARCHIVE_USER,
+            "api_number": 0,
+            "auth_data": get_tg_auth_data()
+        }
+        req_body_str = json.dumps(req_body, separators=(",", ":"))
+        req_headers = {
+            "Content-Length": str(len(req_body_str)),
+            "Content-Type": "application/json",
+            "Origin": "https://tgarchive.eu.org",
+            "Referer": "https://tgarchive.eu.org"
+        }
+        auth_response = requests.post(url=TG_ARCHIVE_AUTH_API, data=json.dumps(req_body), headers=req_headers)
+        if auth_response.ok:
+            logger.info(f"Received response:: {auth_response.text}")
+            auth_token = json.loads(auth_response.content).get("token")
+        else:
+            logger.error(f"Received failure response:: {auth_response.text}")
+    except requests.exceptions.RequestException as err:
+        err_msg = err.response.text if hasattr(err, 'response') else err.__class__.__name__
+        logger.error(f"Failed to fetch auth token for tgarchive, error:: {err_msg}")
+    except json.JSONDecodeError as err:
+        logger.error(f"Error while parsing json data:: {err.msg}")
+    return auth_token
+
+def fetch_tgarchive_files(query: str, retry_count: int = 1) -> Optional[Dict]:
+    global TG_ARCHIVE_TOKEN
+    results: Optional[Dict] = dict()
+    if not TG_ARCHIVE_SEARCH_API:
+        logger.warning("Required param for searching tgarchive is missing")
+        return results
+    if not TG_ARCHIVE_TOKEN:
+        if not (TG_ARCHIVE_TOKEN := fetch_tgarchive_token()):
+            logger.error("Unable to get tgarchive token")
+            return results
+    logger.info(f"Fetching files from:: {TG_ARCHIVE_SEARCH_API}")
+    try:
+        req_body = {
+            "site": "tgarchive",
+            "q": query,
+            "pageToken": None,
+            "api_number": 0,
+            "auth_data": get_tg_auth_data()
+        }
+        req_body_str = json.dumps(req_body, separators=(",", ":"))
+        req_headers = {
+            "Authorization": f"Bearer {TG_ARCHIVE_TOKEN}",
+            "Content-Length": str(len(req_body_str)),
+            "Content-Type": "text/plain;charset=UTF-8",
+            "Origin": "https://tgarchive.eu.org",
+            "Referer": "https://tgarchive.eu.org"
+        }
+        files_response = requests.post(url=TG_ARCHIVE_SEARCH_API, data=json.dumps(req_body), headers=req_headers)
+        if files_response.ok:
+            logger.debug(f"Received response:: {files_response.text}")
+            files_list = json.loads(files_response.content).get("files")
+            for _file in files_list:
+                _file_obj = json.loads(_file.get("description"))
+                results[str(int(_file_obj.get("id")))] = {
+                    "name": _file_obj.get("name"),
+                    "size": humanize.naturalsize(_file_obj.get("size")),
+                    "mimeType": _file_obj.get("mime_type"),
+                    "is_tgapi": True
+                }
+        else:
+            logger.error(f"Received failure response:: {files_response.text}")
+            if json.loads(files_response.text).get("message") == "Unauthorized":
+                logger.warning("Token is expired")
+                TG_ARCHIVE_TOKEN = None
+                if retry_count <= 1:
+                    results = fetch_tgarchive_files(query, retry_count+1)
+    except (requests.exceptions.RequestException, AttributeError) as err:
+        err_msg = err.response.text if hasattr(err, 'response') else err.__class__.__name__
+        logger.error(f"Failed to fetch files from tgarchive, error:: {err_msg}")
+    except json.JSONDecodeError as err:
+        logger.error(f"Error while parsing json data:: {err.msg}")
+    return results
 
 @flask_app.get("/search/<query>")
-def search(query: str) -> dict:
+def search(query: str):
     query = unquote(query)
-    query = re.sub('[^a-zA-Z0-9-_. ]', '', query)
+    query = re.sub('[^a-zA-Z0-9-_ ]', '', query)
     logger.info(f"Searching: {query}")
     quality = None
-    for res in QUALITIES:
-        if re.search(res, query):
-            quality = res
+    for resol in QUALITIES:
+        if re.search(resol, query):
+            quality = resol
             break
-    count = 0
-    files_md5 = set()
-    response_data = dict()
     try:
-        creds = get_oauth_creds()
-        service = build('drive', 'v3', credentials=creds, cache_discovery=False)
+        if os.path.exists(PICKLE_FILE):
+            with open(PICKLE_FILE, 'rb') as f:
+                credentials = pickle.load(f)
+                if credentials and credentials.expired and credentials.refresh_token:
+                    try:
+                        credentials.refresh(Request())
+                    except RefreshError:
+                        logger.error("Failed to refresh token")
+                        raise GdriveSearchError
+        else:
+            err_msg = f"{PICKLE_FILE} not found"
+            logger.error(err_msg)
+            raise FileNotFoundError(err_msg)
+        service = build('drive', 'v3', credentials=credentials, cache_discovery=False)
         gdrive_query = "mimeType != 'application/vnd.google-apps.folder' and "
-        query_list = re.split(" ", query)
+        query_list = re.split(' ', query)
         for text in query_list:
             if quality is not None and re.search(quality, text):
                 continue
@@ -142,34 +242,38 @@ def search(query: str) -> dict:
                                        q=gdrive_query, corpora='allDrives', spaces='drive',
                                        fields='files(id, name, mimeType, size, md5Checksum)').execute()["files"]
         service.close()
+        count = 0
+        response_dict = dict()
+        resultsMap = dict()
         for file in results:
-            md5 = file.get('md5Checksum')
             if count >= MAX_RESULTS:
                 break
-            elif REMOVE_DUPLICATES is False and md5 in files_md5:
+            elif file.get('mimeType') == 'application/vnd.google-apps.shortcut' \
+                    or contains(query_list, file.get('name').lower()) is False:
                 continue
             else:
-                files_md5.add(md5)
-            if file.get('mimeType') == 'application/vnd.google-apps.shortcut' or contains(query_list, file.get('name').lower()) is False:
-                continue
-            response_data[file.get('id')] = {'name': file.get('name'), 'size': f"{humanize.naturalsize(file.get('size', 0))}", 'mimeType': file.get('mimeType')}
-            count += 1
-        asyncio.run(check_download(creds, response_data))
-        logger.info(f"Query: {query}, Found: {len(response_data)}")
-        response_data['status'] = 'success'
-        return response_data
-    except Exception as err:
-        logger.error(f"Failed to search:{query} error: {str(err)}")
-        return {'status': 'error', 'msg': f'Internal Server Error[{str(err)}]'}
+                resultsMap[file.get('id')] = {'name': file.get('name'), 'size': f"{humanize.naturalsize(file.get('size', 0))}", 'mimeType': file.get('mimeType'), 'is_tgapi': False}
+                count += 1
+
+        if len(resultsMap) < 10 and (tg_files := fetch_tgarchive_files(query)):
+            resultsMap.update(tg_files)
+
+        asyncio.run(check_download(credentials, resultsMap))
+        logger.info(f"Query: {query}, Found: {len(resultsMap)}")
+        response_dict['files'] = dict(natsorted(seq=resultsMap.items(), key=lambda x: x[1].get('name')))
+        response_dict['status'] = 'success'
+        return response_dict
+    except Exception as e:
+        return {'status': 'error', 'msg': f'Internal Server Error[{e.__class__.__name__}: {str(e)}]'}
 
 @flask_app.get("/titles/<query>")
-def titles(query: str) -> dict:
+def titles(query: str):
     results = dict()
     title = []
     try:
         ia = Cinemagoer()
         query = unquote(query)
-        logger.info(f"Fetching titles for: {query}")
+        logger.info(f"Getting titles for: {query}")
         for movie in ia.search_movie(title=query, results=10):
             name = movie.data.get('title').strip()
             year = movie.data.get('year')
@@ -177,7 +281,7 @@ def titles(query: str) -> dict:
             if year is not None and kind == 'movie':
                 name += f" {str(year)}"
             title.append(name)
-        results['titles'] = title
+        results['titles'] = list(set(title))
         results['status'] = 'success'
     except Exception:
         results['status'] = 'error'
